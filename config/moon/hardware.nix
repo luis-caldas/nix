55e9c3af21e224dbe2e82b2861eb0dbb3c6bf24b
Luis <admin@luiscaldas.com>
2020-07-20 18:29:23 +0100

    added example code for vfio pci

--- config/moon/hardware.nix
+++ config/moon/hardware.nix
@@ -9,9 +9,19 @@
 
     # Load the proper modules and disks in the initrd
     initrd = {
-      availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+      availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "vfio-pci"];
       kernelModules = [];
     };
+
+    kernelParams = [ "pcie_aspm=off" "amd_iommu=on" ];
+
+    #initrd.preDeviceCommands = ''
+    #  devices="0000:0a:00.0 0000:0a:00.1"
+    #  for each_device in $devices; do
+    #    echo "vfio-pci" > /sys/bus/pci/devices/$each_device/driver_override
+    #  done
+    #  modprobe -i vfio-pci
+    #'';
 
     # Extra modules for the kernel
     kernelModules = ["kvm-amd"];
