ebed6d54411a6c7114669a2fcfd434bb29fc8cc3
Luis <admin@luiscaldas.com>
2020-08-17 14:19:13 +0100

    removed firefox

--- config.nix
+++ config.nix
@@ -13,12 +13,8 @@ let
        builtins.readFile (./config + ("/" + system-path) + "/config.json"))
     );
 
-  # Import the firefox config
-  firefox-obj = builtins.fromJSON (builtins.readFile (./config + "/firefox.json"));
-
 in
 {
   path = system-path;
   config = config-obj;
-  firefox = firefox-obj;
 }
