f4d7baba8c80dbe70b174b692c72b5829197938f
Luis <admin@luiscaldas.com>
2020-10-12 15:06:11 +0100

    added a new machine

---
+++ config/square/hardware.nix
@@ -0,0 +1,33 @@
+{ lib, ... }:
+{
+  imports =
+    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ];
+
+  boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ata_piix" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "boxy/root";
+      fsType = "zfs";
+    };
+
+  fileSystems."/home" =
+    { device = "boxy/home";
+      fsType = "zfs";
+    };
+
+  fileSystems."/data" =
+    { device = "boxy/data";
+      fsType = "zfs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/B0C3-ED9F";
+      fsType = "vfat";
+    };
+
+  nix.maxJobs = lib.mkDefault 4;
+}
