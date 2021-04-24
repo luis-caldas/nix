6c873e3220bf468636e1ef8e24de024b0ea7d680
Luis <admin@luiscaldas.com>
2021-04-24 11:52:53 +0100

    added audio production fixed

--- linker.nix
+++ linker.nix
@@ -87,6 +87,8 @@ let
   ] [] ++
   # Check if audio is supported
   mfunc.useDefault my.config.audio [ ./common/system/audio.nix ] [] ++
+  # Check if audio production is set
+  mfunc.useDefault my.config.graphical.production [ ./common/system/production.nix ] [] ++
   # Check if bluetooth is supported
   mfunc.useDefault my.config.bluetooth [ ./common/system/bluetooth.nix ] [] ++
   # Bluetooth and video are enabled
