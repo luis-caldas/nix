65c8ce2934e319361cfafdc82562072a3c458326
Luis <admin@luiscaldas.com>
2021-06-19 13:49:21 +0100

    changed iso to new nix options

--- iso/iso.nix
+++ iso/iso.nix
@@ -24,7 +24,7 @@ in {
   # Override some of the iso configurations
   # So my ones may work on top
   networking.wireless.enable = false;
-  services.mingetty.autologinUser = lib.mkForce null;
+  services.getty.autologinUser = lib.mkForce null;
   # Boot loader configs should follow the iso
   boot.loader.timeout = lib.mkForce null;
 
