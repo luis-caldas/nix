7ef292c90fb7e74b68024655d9646ba1d9928af5
Luis <admin@luiscaldas.com>
2021-07-03 15:17:41 +0100

    added some opencl drivers

--- common/system/video/video.nix
+++ common/system/video/video.nix
@@ -21,7 +21,12 @@
   hardware.opengl = {
     enable = true;
     driSupport32Bit = true;
-    extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+    extraPackages32 = with pkgs; [
+      intel-ocl
+      rocm-opencl-icd
+      rocm-opencl-runtime
+      pkgsi686Linux.libva
+    ];
     # Select custom version of mesa drivers
     #package = pkgs.mesa.drivers;
   };
