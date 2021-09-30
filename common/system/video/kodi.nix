e2a7aeeac149ebed8b914304343f367bd764afd2
Luis <admin@luiscaldas.com>
2021-09-30 13:44:14 +0100

    updated kodi packages

--- common/system/video/kodi.nix
+++ common/system/video/kodi.nix
@@ -1,10 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 {
 
   # Needed xserver configs for kodi
   services.xserver.enable = true;
-  services.xserver.desktopManager.kodi.enable = true;
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "kodi";
+
+  # Enable kodi
+  services.xserver.desktopManager.kodi.enable = true;
+
+  # Kodi with packages
+  services.xserver.desktopManager.kodi.package = pkgs.kodi.withPackages (p: with p; [
+    inputstream-rtmp inputstreamhelper inputstream-adaptive inputstream-ffmpegdirect
+    six kodi-six idna urllib3 chardet certifi requests myconnpy dateutil
+    pvr-iptvsimple pvr-hts pvr-hdhomerun
+    youtube netflix
+    a4ksubtitles
+    pdfreader
+    osmc-skin
+    joystick steam-launcher
+  ]);
 
 }
