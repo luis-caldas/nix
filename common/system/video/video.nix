03dd296fc4388f2d888e7b0f3c3b4bdd71eaacfd
Luis <admin@luiscaldas.com>
2021-09-30 13:26:51 +0100

    added graphics updated

--- common/system/video/video.nix
+++ common/system/video/video.nix
@@ -4,15 +4,22 @@
   # Set graphics drivers
   services.xserver.videoDrivers = my.config.graphical.drivers;
 
-  # Add 32 bit support
+  # Add 32 bit support and other acceleration packages
   hardware.opengl = {
     enable = true;
     driSupport32Bit = true;
     extraPackages32 = with pkgs; [
       intel-ocl
+      vaapiIntel
       rocm-opencl-icd
       rocm-opencl-runtime
       pkgsi686Linux.libva
+    ];
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
     ];
     # Select custom version of mesa drivers
     #package = pkgs.mesa.drivers;
