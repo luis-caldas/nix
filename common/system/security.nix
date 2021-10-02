63acb83361adc2e50b6839f9721c61be0a9af3e0
Luis <admin@luiscaldas.com>
2021-10-02 19:00:23 +0100

    updated polkit permissions login1

--- common/system/security.nix
+++ common/system/security.nix
@@ -14,25 +14,21 @@
   security.polkit.extraConfig = ''
 
     polkit.addRule(function(action, subject) {
-      if (action.id.indexOf("org.freedesktop.login1.hibernate") == 0) {
-        return polkit.Result.AUTH_ADMIN;
-      }
-    });
-
-    polkit.addRule(function(action, subject) {
-      if (action.id.indexOf("org.freedesktop.login1.power-off") == 0) {
-        return polkit.Result.AUTH_ADMIN;
-      }
-    });
-
-    polkit.addRule(function(action, subject) {
-      if (action.id.indexOf("org.freedesktop.login1.reboot") == 0) {
-        return polkit.Result.AUTH_ADMIN;
-      }
-    });
-
-    polkit.addRule(function(action, subject) {
-      if (action.id.indexOf("org.freedesktop.login1.suspend") == 0) {
+      if ((action.id.indexOf("org.freedesktop.login1.power-off") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.power-off-multiple-sessions") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.power-off-ignore-inhibit") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.reboot") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.reboot-multiple-sessions") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.reboot-ignore-inhibit") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.halt") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.halt-multiple-sessions") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.halt-ignore-inhibit") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.suspend") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.suspend-multiple-sessions") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.suspend-ignore-inhibit") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.hibernate") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.hibernate-multiple-sessions") == 0) ||
+          (action.id.indexOf("org.freedesktop.login1.hibernate-ignore-inhibit") == 0)) {
         return polkit.Result.AUTH_ADMIN;
       }
     });
