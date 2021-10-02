60b3d18f99ff1c35c1bd8eff16fd52fa27b85c42
Luis <admin@luiscaldas.com>
2021-10-02 18:05:23 +0100

    added power commands only for su

--- common/system/security.nix
+++ common/system/security.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ my, mfunc, ... }:
 {
 
   # Create the sudo group
@@ -9,6 +9,35 @@
     enable = true;
     extraConfig = "%sudo	ALL=(ALL:ALL)	NOPASSWD: ALL";
   };
+
+  # Disable power commands if not su and if it was configured
+  security.polkit.extraConfig = mfunc.useDefault (!my.config.system.power) ''
+
+    polkit.addRule(function(action, subject) {
+      if (action.id.indexOf("org.freedesktop.login1.hibernate") == 0) {
+        return polkit.Result.AUTH_ADMIN;
+      }
+    });
+
+    polkit.addRule(function(action, subject) {
+      if (action.id.indexOf("org.freedesktop.login1.power-off") == 0) {
+        return polkit.Result.AUTH_ADMIN;
+      }
+    });
+
+    polkit.addRule(function(action, subject) {
+      if (action.id.indexOf("org.freedesktop.login1.reboot") == 0) {
+        return polkit.Result.AUTH_ADMIN;
+      }
+    });
+
+    polkit.addRule(function(action, subject) {
+      if (action.id.indexOf("org.freedesktop.login1.suspend") == 0) {
+        return polkit.Result.AUTH_ADMIN;
+      }
+    });
+
+  '' "";
 
   programs.ssh = {
     # Disable askpass graphical password program
