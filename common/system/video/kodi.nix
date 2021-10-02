6719c44d74497bb6a48175fbe45c85fb3953dd7f
Luis <admin@luiscaldas.com>
2021-10-02 20:47:18 +0100

    added cleanupscript for lightdm

--- common/system/video/kodi.nix
+++ common/system/video/kodi.nix
@@ -6,6 +6,11 @@
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "kodi";
 
+  # Cleanup code for lightdm (default displaymanager)
+  services.xserver.displayManager.lightdm.extraConfig = ''
+    session-cleanup-script=pkill -P1 -fx ${pkgs.lightdm}/bin/lightdm
+  '';
+
   # Enable kodi
   services.xserver.desktopManager.kodi.enable = true;
 
