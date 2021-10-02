9eefbd217aea2dd9bede09c6771a2463cf17d85a
Luis <admin@luiscaldas.com>
2021-10-02 20:17:01 +0100

    lightdm is already deafult

--- common/system/video/kodi.nix
+++ common/system/video/kodi.nix
@@ -3,9 +3,8 @@
 
   # Needed xserver configs for kodi
   services.xserver.enable = true;
-  services.xserver.displayManager.lightdm.autoLogin.enable = true;
-  services.xserver.displayManager.lightdm.autoLogin.user = "kodi";
-  services.xserver.displayManager.lightdm.greeter.enable = false;
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "kodi";
 
   # Enable kodi
   services.xserver.desktopManager.kodi.enable = true;
