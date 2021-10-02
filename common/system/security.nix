ac2548e1f17a45b30c6eaba4b0aefb3f4da86a74
Luis <admin@luiscaldas.com>
2021-10-02 19:51:03 +0100

    added more locked polkit rules and compressed them

--- common/system/security.nix
+++ common/system/security.nix
@@ -12,27 +12,15 @@
 
   # Disable power commands if not super user or on wheel group
   security.polkit.extraConfig = ''
-
     polkit.addRule(function(action, subject) {
-      if ((action.id.indexOf("org.freedesktop.login1.power-off") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.power-off-multiple-sessions") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.power-off-ignore-inhibit") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.reboot") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.reboot-multiple-sessions") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.reboot-ignore-inhibit") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.halt") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.halt-multiple-sessions") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.halt-ignore-inhibit") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.suspend") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.suspend-multiple-sessions") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.suspend-ignore-inhibit") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.hibernate") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.hibernate-multiple-sessions") == 0) ||
-          (action.id.indexOf("org.freedesktop.login1.hibernate-ignore-inhibit") == 0)) {
+      if ((action.id.indexOf("org.freedesktop.login1.") == 0) ||
+          (action.id.indexOf("org.freedesktop.upower.") == 0) ||
+          (action.id.indexOf("org.freedesktop.udisks.") == 0) ||
+          (action.id.indexOf("org.freedesktop.udisks2.") == 0) ||
+          (action.id.indexOf("org.freedesktop.consolekit.system.") == 0)) {
         return polkit.Result.AUTH_ADMIN;
       }
     });
-
   '';
 
   programs.ssh = {
