7ce01d886cf088f954a77d57e62cdf32167ede14
Luis <admin@luiscaldas.com>
2021-10-08 15:30:21 +0100

    updated pinebook firmware and kernel

--- config/lap/custom/overlay.nix
+++ config/lap/custom/overlay.nix
@@ -1,41 +1,19 @@
 final: super:
+
 let
   inherit (final) callPackage kernelPatches linuxPackagesFor;
 in
 {
-  # Alternative BSP u-boot, with nvme support if desired
-  #   * https://gitlab.manjaro.org/manjaro-arm/packages/core/uboot-pinebookpro
-  uBootPinebookPro = callPackage ./u-boot {};
-  uBootPinebookProExternalFirst = callPackage ./u-boot {
-    externalFirst = true;
-  };
-  linux_pinebookpro = callPackage ./kernel {
-    kernelPatches = [
-      kernelPatches.bridge_stp_helper
-      #kernelPatches.export_kernel_fpu_functions
-      {
-        name = "pinebookpro-config-fixes";
-        patch = null;
-        extraConfig = ''
-          PCIE_ROCKCHIP y
-          PCIE_ROCKCHIP_HOST y
-          PCIE_DW_PLAT y
-          PCIE_DW_PLAT_HOST y
-          PHY_ROCKCHIP_PCIE y
-          PHY_ROCKCHIP_INNO_HDMI y
-          PHY_ROCKCHIP_DP y
-          ROCKCHIP_MBOX y
-          STAGING_MEDIA y
-          VIDEO_HANTRO y
-          VIDEO_HANTRO_ROCKCHIP y
-          USB_DWC2_PCI y
-          ROCKCHIP_LVDS y
-          ROCKCHIP_RGB y
-        '';
-      }
-    ];
-  };
-  linuxPackages_pinebookpro = linuxPackagesFor final.linux_pinebookpro;
-  pinebookpro-firmware = callPackage ./firmware {};
+  # The unqualified kernel attr is deprecated.
+  linux_pinebookpro = throw "The linux_pinebookpro attribute has been replaced by linux_pinebookpro_latest.";
+  linuxPackages_pinebookpro = throw "The linuxPackages_pinebookpro attribute has been replaced by linuxPackages_pinebookpro_latest.";
+
+  linux_pinebookpro_latest = callPackage ./kernel/latest { kernelPatches = []; };
+  linuxPackages_pinebookpro_latest = linuxPackagesFor final.linux_pinebookpro_latest;
+
+  linux_pinebookpro_lts = callPackage ./kernel/lts { kernelPatches = []; };
+  linuxPackages_pinebookpro_lts = linuxPackagesFor final.linux_pinebookpro_lts;
+
+  pinebookpro-ap6256-firmware = callPackage ./firmware/ap6256-firmware.nix {};
   pinebookpro-keyboard-updater = callPackage ./keyboard-updater {};
 }
