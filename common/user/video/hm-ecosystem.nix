ad573c2501250aaac313ee29fa010305083ad1ab
Luis <admin@luiscaldas.com>
2021-11-06 18:43:08 +0000

    restored remote fonts on chromium

--- common/user/video/hm-ecosystem.nix
+++ common/user/video/hm-ecosystem.nix
@@ -267,7 +267,7 @@ in
   programs.chromium = {
     enable = true;
     package = pkgs.chromium.override {
-      commandLineArgs = "--disable-remote-fonts --force-dark-mode --enable-features=WebUIDarkMode";
+      commandLineArgs = "--force-dark-mode --enable-features=WebUIDarkMode";
     };
   };
 
