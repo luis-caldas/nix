d2451946db75421a982f0978f06adbfa3e977cce
Luis <admin@luiscaldas.com>
2022-01-07 15:39:36 +0000

    added a new system

---
+++ config/router/hardware.nix
@@ -0,0 +1,42 @@
+{ lib, config, pkgs, ... }:
+{
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "vimmer/root";
+      fsType = "zfs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/B7F0-3A34";
+      fsType = "vfat";
+    };
+
+  fileSystems."/home" =
+    { device = "vimmer/home";
+      fsType = "zfs";
+    };
+
+  fileSystems."/data/vms" =
+    { device = "vimmer/vms";
+      fsType = "zfs";
+    };
+
+  fileSystems."/nix" =
+    { device = "vimmer/nix";
+      fsType = "zfs";
+    };
+
+  fileSystems."/tmp" =
+    { device = "vimmer/tmp";
+      fsType = "zfs";
+    };
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+}
