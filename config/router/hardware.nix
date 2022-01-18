3976edc644e61a1d9b8ab552db5e5421d2947009
Luis <admin@luiscaldas.com>
2022-01-18 17:41:12 +0000

    added vfio to router

--- config/router/hardware.nix
+++ config/router/hardware.nix
@@ -1,10 +1,19 @@
 { lib, config, pkgs, ... }:
 {
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "vfio-pci" ];
   boot.initrd.kernelModules = [ ];
+  boot.kernelParams = [ "pcie_aspm=off" "amd_iommu=on" "iommu=pt" "pci=noaer" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+
+  boot.initrd.preDeviceCommands = ''
+    devices="0000:07:00.0 0000:07:00.1"
+    for each_device in $devices; do
+      echo "vfio-pci" > /sys/bus/pci/devices/$each_device/driver_override
+    done
+    modprobe -i vfio-pci
+  '';
 
   fileSystems."/" =
     { device = "vimmer/root";
