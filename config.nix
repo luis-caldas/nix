840dfea585d9ae4d35e15960d75c3f2753312f2e
Luis <admin@luiscaldas.com>
2022-01-18 16:22:37 +0000

    version froze my projects

--- config.nix
+++ config.nix
@@ -30,36 +30,41 @@ let
 
   # Create fetch project function
   fetchProject =
-    projectName:
+    projectData:
       let
-        githubUrlBuilder = userString: repoString:
-        let
-          protocol = "https";
-          domain = "github.com";
-        in
-          protocol + "://" + domain + "/" + userString + "/" + repoString;
+        owner = "luis-caldas";
       in
-        builtins.fetchGit (githubUrlBuilder "luis-caldas" projectName);
+        pkgs.fetchFromGitHub rec { inherit owner; inherit (projectData) repo rev sha256; name = replaceName repo; };
 
   # Link all my projects to the config
-  listProjects = [
-    "myconky"
-    "mythemes"
-    "myfonts"
-    "mycursors"
-    "myicons"
-    "mywallpapers"
-    "myshell"
-    "myvim"
-  ];
+  listSomeProjects = {
+    "myconky"      = { rev = "2c36fc74b49a68c083a8f01d363fb6fdae5fd5fb"; sha256 = "0554vfsfy5sh3gjgvgxmj90baj161hy9ala15j2qqv5v18zyc5yh"; };
+    "mythemes"     = { rev = "e55cec84f30af58e2aa36af3145167167cc9fce4"; sha256 = "1kfwqwpai0yk2i4xq5zgvfy7s5gxbvy5ckqsq3ai6ppz5f4pb2is"; };
+    "myfonts"      = { rev = "3623109a3f342622db3f9a42bf906b898777abe2"; sha256 = "1rlb6wf5i4vvcc4m29796kr18zxi9040x21in0kf4kppmzxliy94"; };
+    "mycursors"    = { rev = "42b16a3d8b2af5b45ca57df3e4e0c07bd5cd19e9"; sha256 = "10nzdpdvp4pwz3w2z980v137yzdj8qwhkr2fwz46vram9400xfv7"; };
+    "myicons"      = { rev = "fd4692ed166aab23865a45ee154bf23cadb843e9"; sha256 = "017pqsqhln6hjs2ini4f9fabf7smj628dyavr2w7566vv13hqni6"; };
+    "mywallpapers" = { rev = "4c9dcbe6f58a123dacb7bc9d10f95761efb4e642"; sha256 = "0j5dym6a10xxqxd6mpqsssbc3wd77hkv29yf6agh6wg7yfjck5rk"; };
+    "myshell"      = { rev = "8f91a5724fed1951cff78c3cfb29276d4f284fc2"; sha256 = "1qhahq9dvc9qv0c1vs4aardmpsak35z3l6bz8bvl7g31ink0n1ks"; };
+    "myvim"        = { rev = "4469988257a52021d98486d9bc053f582f28b97e"; sha256 = "0g30vjzqmhb7i0kcz8nrgbhw5xx4lfh6fn0hspdd7dcx7x10qfc1"; };
+  };
   someProjects = builtins.listToAttrs (map (
-    eachProject: { name = (replaceName eachProject); value = (fetchProject eachProject); }
-  ) listProjects);
+    eachProjectName: {
+      name = replaceName eachProjectName;
+      value = fetchProject {
+        repo = eachProjectName;
+        inherit (listSomeProjects."${eachProjectName}") rev sha256;
+      };
+    }
+  ) (builtins.attrNames listSomeProjects));
 
   # Ovewrite desktop project with derivated subfolders
   desktopProject = let
-    myProject = "mydesktop";
-    fixedName = replaceName myProject;
+    myProject = {
+      repo = "mydesktop";
+      rev = "6c5cba2f2941ec098e4fb0271c5435418838dcd2";
+      sha256 = "1968rhf7cbrn35kr00fgx3w135lx39kb7v9v62803iqhqv8k6gsq";
+    };
+    fixedName = replaceName myProject.repo;
     fetchedProject = fetchProject myProject;
     subFolders = lib.remove null (lib.mapAttrsToList (
       name: value:
