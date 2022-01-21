578e6c70a451d32c97a9f8ec569b9475b7595524
Luis <admin@luiscaldas.com>
2022-01-21 23:38:49 +0000

    changed order of projects to be alphabetical

--- config.nix
+++ config.nix
@@ -38,14 +38,14 @@ let
 
   # Link all my projects to the config
   listSomeProjects = {
-    "myconky"      = { rev = "2c36fc74b49a68c083a8f01d363fb6fdae5fd5fb"; sha256 = "0554vfsfy5sh3gjgvgxmj90baj161hy9ala15j2qqv5v18zyc5yh"; };
-    "mythemes"     = { rev = "e55cec84f30af58e2aa36af3145167167cc9fce4"; sha256 = "1kfwqwpai0yk2i4xq5zgvfy7s5gxbvy5ckqsq3ai6ppz5f4pb2is"; };
-    "myfonts"      = { rev = "3623109a3f342622db3f9a42bf906b898777abe2"; sha256 = "1rlb6wf5i4vvcc4m29796kr18zxi9040x21in0kf4kppmzxliy94"; };
+    "myconky"      = { rev = "1b494d50c2bd789250841dcc54aaf733ebc84550"; sha256 = "06arv0a3i1q1ya5i38y55qc0zs96v7mqkkr5ha6aih6xj92yj6cx"; };
     "mycursors"    = { rev = "42b16a3d8b2af5b45ca57df3e4e0c07bd5cd19e9"; sha256 = "10nzdpdvp4pwz3w2z980v137yzdj8qwhkr2fwz46vram9400xfv7"; };
+    "myfonts"      = { rev = "3623109a3f342622db3f9a42bf906b898777abe2"; sha256 = "1rlb6wf5i4vvcc4m29796kr18zxi9040x21in0kf4kppmzxliy94"; };
     "myicons"      = { rev = "fd4692ed166aab23865a45ee154bf23cadb843e9"; sha256 = "017pqsqhln6hjs2ini4f9fabf7smj628dyavr2w7566vv13hqni6"; };
-    "mywallpapers" = { rev = "4c9dcbe6f58a123dacb7bc9d10f95761efb4e642"; sha256 = "0j5dym6a10xxqxd6mpqsssbc3wd77hkv29yf6agh6wg7yfjck5rk"; };
-    "myshell"      = { rev = "8f91a5724fed1951cff78c3cfb29276d4f284fc2"; sha256 = "1qhahq9dvc9qv0c1vs4aardmpsak35z3l6bz8bvl7g31ink0n1ks"; };
+    "myshell"      = { rev = "3ef9ca1b06141eda5f25a65293e9c319c81ba286"; sha256 = "0md3sg8s9a1gxb49c08x243b3l7galz8qalp7ibx28ghfgx6cy0p"; };
+    "mythemes"     = { rev = "e55cec84f30af58e2aa36af3145167167cc9fce4"; sha256 = "1kfwqwpai0yk2i4xq5zgvfy7s5gxbvy5ckqsq3ai6ppz5f4pb2is"; };
     "myvim"        = { rev = "4469988257a52021d98486d9bc053f582f28b97e"; sha256 = "0g30vjzqmhb7i0kcz8nrgbhw5xx4lfh6fn0hspdd7dcx7x10qfc1"; };
+    "mywallpapers" = { rev = "4c9dcbe6f58a123dacb7bc9d10f95761efb4e642"; sha256 = "0j5dym6a10xxqxd6mpqsssbc3wd77hkv29yf6agh6wg7yfjck5rk"; };
   };
   someProjects = builtins.listToAttrs (map (
     eachProjectName: {
