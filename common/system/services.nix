f14c6fd752953b6602a17b598571448c24385dde
Luis <admin@luiscaldas.com>
2022-03-15 21:36:31 +0000

    added geolocation services and moved overrides

--- common/system/services.nix
+++ common/system/services.nix
@@ -35,30 +35,14 @@
     };
   } {};
 
+  # Geolocation and times
+  services.geoclue2.enable = true;
+  services.localtime.enable = true;
+
   # PCSC
   services.pcscd = {
     enable = true;
     plugins = [ pkgs.acsccid ];
-  };
-
-  # Overrides
-  nixpkgs.config.packageOverrides = pkgs: {
-    # Add custom image to OVMF UEFI
-    OVMFFull = pkgs.OVMFFull.overrideAttrs (attrs: {
-        name = attrs.name + "-custom-logo";
-        postPatch = (if (builtins.hasAttr "postPatch" attrs) then attrs.postPatch else "") + ''
-          "${pkgs.ffmpeg}/bin/ffmpeg" -i "${my.projects.wallpapers}/papes/dpm-navy-small.png" -pix_fmt rgb24 -y -vf scale=256:-1 "./MdeModulePkg/Logo/Logo.bmp"
-        '';
-    });
-    fprintd = mpkgs.fprintd-clients;
-    # Fix Intel OCL URL
-    intel-ocl = pkgs.intel-ocl.overrideAttrs (oldAttrs: {
-      src = pkgs.fetchzip {
-        url = "http://registrationcenter-download.intel.com/akdlm/irc_nas/11396/SRB5.0_linux64.zip";
-        sha256 = "0qbp63l74s0i80ysh9ya8x7r79xkddbbz4378nms9i7a0kprg9p2";
-        stripRoot = false;
-      };
-    });
   };
 
   # Fingerprint
