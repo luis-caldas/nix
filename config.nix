5495245d2ab057f16e697aea47d45dbe5183be3c
Luis <admin@luiscaldas.com>
2022-04-23 14:36:22 +0100

    updated myshell

--- config.nix
+++ config.nix
@@ -42,7 +42,7 @@ let
     "mycursors"    = { rev = "42b16a3d8b2af5b45ca57df3e4e0c07bd5cd19e9"; sha256 = "10nzdpdvp4pwz3w2z980v137yzdj8qwhkr2fwz46vram9400xfv7"; };
     "myfonts"      = { rev = "b2536dff328409cd7b42422cfbdadb92dba28bdc"; sha256 = "0vbgxzyimp90d0izwg4s3g4f6dl1d8c2chb869s2fynp9z2jfrf0"; };
     "myicons"      = { rev = "fd4692ed166aab23865a45ee154bf23cadb843e9"; sha256 = "017pqsqhln6hjs2ini4f9fabf7smj628dyavr2w7566vv13hqni6"; };
-    "myshell"      = { rev = "bfa4f93f7f0fff7bdb56fea3c6baf778f7bdb924"; sha256 = "1maaskw7rv7ks11inhi5viih6gkyr2p0d08mvvn9jkbjnaxnp17y"; };
+    "myshell"      = { rev = "caaf4490bf9b4221cd2d0d6f8e04861144091700"; sha256 = "13z05gkhn63nnb39lil825w72sd20z70n4bv4j5483l9cc8xv1d0"; };
     "mythemes"     = { rev = "e55cec84f30af58e2aa36af3145167167cc9fce4"; sha256 = "1kfwqwpai0yk2i4xq5zgvfy7s5gxbvy5ckqsq3ai6ppz5f4pb2is"; };
     "myvim"        = { rev = "4469988257a52021d98486d9bc053f582f28b97e"; sha256 = "0g30vjzqmhb7i0kcz8nrgbhw5xx4lfh6fn0hspdd7dcx7x10qfc1"; };
     "mywallpapers" = { rev = "42fb2fe79306bf3be52027ea740d53fa19e838af"; sha256 = "10ywkzpigkqk8a040hifqkkndc9cf3sjrdzw8f5lh6v05vbfl2fd"; };
