c9f312f3b4825f1d4f91d6a0000c04c6ca2b5700
Luis <admin@luiscaldas.com>
2022-06-02 18:32:17 +0100

    removed legacy code from kodi

--- config/opti/kodi.nix
+++ config/opti/kodi.nix
@@ -86,14 +86,6 @@
     ]; })
   ];
 
-  # Enable retroarch cores
-  nixpkgs.config.retroarch = {
-    enableFceumm = true;
-    enableSnes9x = true;
-    enableMgba = true;
-    enableMupen64Plus = true;
-  };
-
   # Enable kodi
   services.xserver.desktopManager.kodi.enable = true;
 
