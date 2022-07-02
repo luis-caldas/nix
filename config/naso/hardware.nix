308a25022bffd7218bd9890365f1207c3a52af5e
Luis <admin@luiscaldas.com>
2022-07-02 10:23:42 +0100

    added one more share on naso

--- config/naso/hardware.nix
+++ config/naso/hardware.nix
@@ -46,12 +46,14 @@
         "-S" # Disable minimum SMB2
         "-s" "media;/media;yes;no;yes;all;;;Share for media files" # Share config
         "-s" "google;/google;yes;yes;yes;all;;;Google saved files"
+        "-s" "ps2;/ps2;yes;yes;yes;all;;;PS2 Games"
         "-w" "WORKGROUP" # Default workgroup
         "-W" # Wide link support
       ];
       volumes = [
         "/data/bunker/everything/vault/untouched/google-parents:/google"
         "/data/storr/media:/media"
+        "/data/storr/media/games/roms/ps2:/ps2"
       ];
       ports = [
         "137:137/udp"
