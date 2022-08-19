d540d1c5b2da2a7fd4b209b95f289bc589cf3d5b
Luis <admin@luiscaldas.com>
2022-08-19 18:14:29 +0100

    added new system

---
+++ config/ec2/hardware.nix
@@ -0,0 +1,22 @@
+{ my, lib, config, mfunc, pkgs, modulesPath, ... }:
+{
+
+  imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
+  ec2.hvm = true;
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    permitRootLogin = "no";
+  };
+
+  users.users."${my.config.user.name}".openssh.authorizedKeys.keyFiles = [
+    /etc/nixos/ssh/authorized_keys
+  ];
+
+  swapDevices = [ { device = "/swapfile"; } ];
+
+  system.stateVersion = "22.05";
+
+}
