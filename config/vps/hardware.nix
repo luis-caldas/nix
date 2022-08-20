04d656fd51671b7e7bbc36d8edbc0dfbda0d7fae
Luis <admin@luiscaldas.com>
2022-08-20 12:34:15 +0100

    moved ec2 and removed containers

---
+++ config/vps/hardware.nix
@@ -0,0 +1,33 @@
+{ my, lib, config, modulesPath, ... }:
+{
+
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    permitRootLogin = lib.mkForce "no";
+  };
+
+  users.users."${my.config.user.name}".openssh.authorizedKeys.keyFiles = [
+    /etc/nixos/ssh/authorized_keys
+  ];
+
+  fileSystems."/" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
+
+  swapDevices = [ {
+    device = "/swapfile";
+    size = (1024 * 4); # Size in MB
+  } ];
+
+  system.stateVersion = "22.05";
+
+}
