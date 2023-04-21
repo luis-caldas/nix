0838c2178e8e280ab416e7c9b59f9808034d73da
Luis <admin@luiscaldas.com>
2023-04-21 18:23:50 +0100

    added new service for vps

--- config/vps/hardware.nix
+++ config/vps/hardware.nix
@@ -1,19 +1,26 @@
-{ my, lib, config, modulesPath, ... }:
+{ my, lib, config, modulesPath, pkgs, ... }:
 {
 
+  # Needed for virutalisation
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
+  # Boot information
   boot.loader.grub.device = "/dev/sda";
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
   boot.initrd.kernelModules = [ "nvme" ];
 
+  # DNS servers
   networking.networkmanager.insertNameservers = [ "1.1.1.1" "1.0.0.1" ];
 
+  # Firewall setup
   networking.firewall = {
     enable = lib.mkForce true;
-    allowedTCPPorts = [ 22 ];
+    allowedTCPPorts = [
+      22    # SSH port
+      80    # Free port
+      7000  # Reverse proxy
+    ];
   };
-
   services.fail2ban = {
     enable = true;
     maxretry = 5;
@@ -22,17 +29,17 @@
     ];
   };
 
+  # SSH setup
   services.openssh = {
     enable = true;
     passwordAuthentication = false;
     kbdInteractiveAuthentication = false;
     permitRootLogin = lib.mkForce "no";
   };
-
+  # User keys for ssh
   users.users."${my.config.user.name}".openssh.authorizedKeys.keyFiles = [
     /etc/nixos/ssh/authorized_keys
   ];
-
   users.users.lakituen = {
     isNormalUser  = true;
     home = "/home/lakituen";
@@ -41,11 +48,24 @@
     ];
   };
 
+  # Custom service
+  systemd.services.frpd = {
+    description = "Fast Reverse Proxy Server";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    restartIfChanged = true;
+    environment = [ "/safe/env/frp.env" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.frp}/bin/frps --config ${./frps.ini}";
+      Restart = "always";
+    };
+  };
+
+  # File systems and SWAP
   fileSystems."/" = {
     device = "/dev/sda1";
     fsType = "ext4";
   };
-
   swapDevices = [ {
     device = "/swapfile";
     size = (1024 * 4); # Size in MB
