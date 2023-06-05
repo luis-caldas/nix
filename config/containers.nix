712f3a864f755f5408ac59d65c2eb9943c403835
Luis <luis@caldas.ie>
2023-06-05 15:25:30 +0100

    added more proxy configs on nginx creator

--- config/containers.nix
+++ config/containers.nix
@@ -321,6 +321,9 @@ let
               ssl_certificate ${certPath};
               ssl_certificate_key ${keyPath};
               location / {
+                  proxy_set_header Host $host;
+                  proxy_set_header X-Real-IP $remote_addr;
+                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
                   proxy_pass http://${info.net.ip}:${info.net.port};
               }
           }
