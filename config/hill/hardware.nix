6772a9f03be4c98cb4685be3a40a71e2bce89c16
Luis <luis@caldas.ie>
2023-08-26 17:23:52 +0100

    updated hill

--- config/hill/hardware.nix
+++ config/hill/hardware.nix
@@ -1,7 +1,7 @@
 { lib, config, ... }:
 {
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "uas" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelParams = [ ];
   boot.kernelModules = [ "kvm-amd" "kvmgt" "mdev" "vfio-iommu-type1" ];
@@ -10,50 +10,31 @@
   boot.zfs.requestEncryptionCredentials = true;
 
   fileSystems."/" =
-    { device = "hill/root";
+    { device = "valley/root";
       fsType = "zfs";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/669A-8E94";
+    { device = "/dev/disk/by-uuid/E119-AD3C";
       fsType = "vfat";
     };
 
   fileSystems."/nix" =
-    { device = "hill/nix";
+    { device = "valley/nix";
       fsType = "zfs";
     };
 
   fileSystems."/tmp" =
-    { device = "hill/tmp";
-      fsType = "zfs";
-    };
-
-  # Second disk
-  fileSystems."/safe" =
-    { device = "hill/safe";
+    { device = "valley/tmp";
       fsType = "zfs";
     };
 
   fileSystems."/home" =
-    { device = "mound/home";
+    { device = "valley/home";
       fsType = "zfs";
-      depends = [ "/safe" ];
     };
 
-  fileSystems."/data" =
-    { device = "mound/safe";
-      fsType = "zfs";
-      depends = [ "/safe" ];
-    };
-
-  fileSystems."/store" =
-    { device = "mound/store";
-      fsType = "zfs";
-      depends = [ "/safe" ];
-    };
-
-  swapDevices = [ ];
+    swapDevices = [ { device = "/dev/zvol/valley/swap"; } ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
