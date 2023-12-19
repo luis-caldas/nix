2a3d1781815d3c643675f45c72ac1edb471158a0
Luis <luis@caldas.ie>
2023-12-19 18:34:23 +0000

    went away with the iso folder

--- iso/build-iso.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# get our folder now
-folder_now="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
-
-nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixos-config=iso.nix "$@"
