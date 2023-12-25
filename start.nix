b1c13605566856979a46134e9c1218ce4a44c328
Luis <luis@caldas.ie>
2023-12-25 18:11:42 +0000

    organising folders

--- start.nix
+++ start.nix
@@ -1,14 +1,8 @@
 { ... }:
 {
 
-  # Start point for everything
+  # Start point of everything
   imports = [
-
-    # Default per device configuration and organising
-    start/config.nix
-
-    # General linker that loads all the necessary files
-    start/linker.nix
 
   ];
 
