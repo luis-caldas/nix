134ddf9c3bc6abc02d87a95c60cd32e981335da4
Luis <luis@caldas.ie>
2024-01-07 13:56:51 +0000

    trying to fix service dependency on router

--- systems/router/containers.nix
+++ systems/router/containers.nix
@@ -1,54 +1,63 @@
 { pkgs, lib, config, ... }:
-{
+let
 
-  virtualisation.arion = let
+       ########
+  ### # Config # ###
+       ########
 
-         ########
-    ### # Config # ###
-         ########
-
-    # Configure all the needed networks
-    networks = {
-      ### # Front # ###
-      front  = { name = "front";  subnet = "172.16.10.0/24"; gateway = "172.16.10.1"; };
-      ### # Time # ###
-      time   = { name = "time";   subnet = "172.16.50.0/24"; gateway = "172.16.50.1"; };
-      ### # FreeDNS # ###
-      update = { name = "update"; subnet = "172.16.60.0/24"; gateway = "172.16.60.1"; };
-      ### # DNS # ###
-      dns = {
-        name = "dns";
-        subnet = "172.16.20.0/24"; gateway = "172.16.20.1";
-        # IPs
-        ips = {
-          dns = "172.16.20.11";
-          dnsUp = "172.16.20.10";
-        };
+  # Configure all the needed networks
+  networks = {
+    ### # Front # ###
+    front  = { name = "front";  subnet = "172.16.10.0/24"; gateway = "172.16.10.1"; };
+    ### # Time # ###
+    time   = { name = "time";   subnet = "172.16.50.0/24"; gateway = "172.16.50.1"; };
+    ### # FreeDNS # ###
+    update = { name = "update"; subnet = "172.16.60.0/24"; gateway = "172.16.60.1"; };
+    ### # DNS # ###
+    dns = {
+      name = "dns";
+      subnet = "172.16.20.0/24"; gateway = "172.16.20.1";
+      # IPs
+      ips = {
+        dns = "172.16.20.11";
+        dnsUp = "172.16.20.10";
       };
     };
+  };
 
-    # Configure the needed names
-    names = {
-      # Front
-      front = "front";
-      # Base
-      dns = "dns";
-      dnsUp = "dns-up";
-      time = "time";
-      # Update
-      update = "freedns";
-      # Asterisk
-      asterisk = {
-        app = "asterisk";
-        web = { simple = "asterisk-web-simple"; normal = "asterisk-web-normal"; };
-      };
-      # Monitor
-      nut = "nut";
-      # Web
-      dash = "dash";
+  # Configure the needed names
+  names = {
+    # Front
+    front = "front";
+    # Base
+    dns = "dns";
+    dnsUp = "dns-up";
+    time = "time";
+    # Update
+    update = "freedns";
+    # Asterisk
+    asterisk = {
+      app = "asterisk";
+      web = { simple = "asterisk-web-simple"; normal = "asterisk-web-normal"; };
     };
+    # Monitor
+    nut = "nut";
+    # Web
+    dash = "dash";
+  };
 
-  in {
+  # Naming for the projects
+  projects = {
+    front = "front";
+    base = "base";
+    asterisk = "asterisk";
+    nut = "nut";
+    web = "web";
+  };
+
+in {
+
+  virtualisation.arion = {
 
     #########
     # Front #
@@ -56,6 +65,7 @@
 
     # All services that will serve the front
 
+    projects.front.serviceName = projects.front;
     projects.front.settings = {
 
       # Networking
@@ -94,94 +104,98 @@
     ########
 
     # Projects needed for a basic router functionality
-    projects.base.settings = {
 
-      # Networking
-      networks."${networks.dns.name}" = {
-        name = networks.dns.name;
-        ipam.config = [{ inherit (networks.dns) subnet gateway; }];
-      };
-      networks."${networks.time.name}" = {
-        name = networks.time.name;
-        ipam.config = [{ inherit (networks.time) subnet gateway; }];
-      };
-      networks."${networks.front.name}".external = true;
+    projects.base = {
+      serviceName = projects.base;
+      settings = {
 
-           #####
-      ### # DNS # ###
-           #####
+        # Networking
+        networks."${networks.dns.name}" = {
+          name = networks.dns.name;
+          ipam.config = [{ inherit (networks.dns) subnet gateway; }];
+        };
+        networks."${networks.time.name}" = {
+          name = networks.time.name;
+          ipam.config = [{ inherit (networks.time) subnet gateway; }];
+        };
+        networks."${networks.front.name}".external = true;
 
-      # Upstream DNS server
-      services."${names.dnsUp}" = {
-        build.image = lib.mkForce pkgs.containerImages.dns;
-        service = {
+             #####
+        ### # DNS # ###
+             #####
+
+        # Upstream DNS server
+        services."${names.dnsUp}" = {
+          build.image = lib.mkForce pkgs.containerImages.dns;
+          service = {
+            # Name
+            container_name = names.dnsUp;
+            # Networking
+            networks."${networks.dns.name}".ipv4_address = networks.dns.ips.dnsUp;
+          };
+        };
+
+             ########
+        ### # PiHole # ###
+             ########
+
+        # PiHole
+        services."${names.dns}".service = {
+
+          # Image
+          image = "pihole/pihole:latest";
+
           # Name
-          container_name = names.dnsUp;
+          container_name = names.dns;
+
+          # Environment
+          environment = {
+            TZ = config.mine.system.timezone;
+            DNSMASQ_LISTENING = "all";
+            PIHOLE_DNS_ = networks.dns.ips.dnsUp;
+          };
+          env_file = [ "/data/local/containers/pihole/env/adblock.env" ];
+
+          # Volumes
+          volumes = [
+            "/data/local/containers/pihole/config/etc:/etc/pihole"
+            "/data/local/containers/pihole/config/dnsmasq:/etc/dnsmasq.d"
+          ];
+
           # Networking
-          networks."${networks.dns.name}".ipv4_address = networks.dns.ips.dnsUp;
+          ports = [
+            "53:53/tcp"
+            "53:53/udp"
+          ];
+          dns = [ "127.0.0.1" ];
+          networks = [ networks.dns.name networks.front.name ];
+
         };
-      };
 
-           ########
-      ### # PiHole # ###
-           ########
+             ######
+        ### # Time # ###
+             ######
 
-      # PiHole
-      services."${names.dns}".service = {
-
-        # Image
-        image = "pihole/pihole:latest";
-
-        # Name
-        container_name = names.dns;
-
-        # Environment
-        environment = {
-          TZ = config.mine.system.timezone;
-          DNSMASQ_LISTENING = "all";
-          PIHOLE_DNS_ = networks.dns.ips.dnsUp;
+        services."${names.time}".service = {
+          # Image file
+          image = "simonrupf/chronyd:latest";
+          # Name
+          container_name = names.time;
+          # Environment
+          environment = {
+            TZ = config.mine.system.timezone;
+            NTP_SERVERS = "time.cloudflare.com";
+            ENABLE_NTS = "true";
+          };
+          # Networking
+          ports = [
+            "123:123/udp"
+          ];
+          networks = [ networks.time.name ];
         };
-        env_file = [ "/data/local/containers/pihole/env/adblock.env" ];
 
-        # Volumes
-        volumes = [
-          "/data/local/containers/pihole/config/etc:/etc/pihole"
-          "/data/local/containers/pihole/config/dnsmasq:/etc/dnsmasq.d"
-        ];
-
-        # Networking
-        ports = [
-          "53:53/tcp"
-          "53:53/udp"
-        ];
-        dns = [ "127.0.0.1" ];
-        networks = [ networks.dns.name networks.front.name ];
 
       };
-
-           ######
-      ### # Time # ###
-           ######
-
-      services."${names.time}".service = {
-        # Image file
-        image = "simonrupf/chronyd:latest";
-        # Name
-        container_name = names.time;
-        # Environment
-        environment = {
-          TZ = config.mine.system.timezone;
-          NTP_SERVERS = "time.cloudflare.com";
-          ENABLE_NTS = "true";
-        };
-        # Networking
-        ports = [
-          "123:123/udp"
-        ];
-        networks = [ networks.time.name ];
-      };
-
-
     };
 
     ##########
@@ -222,48 +236,69 @@
     # Asterisk #
     ############
 
-    projects.asterisk.settings = {
+    projects.asterisk = {
+      serviceName = projects.asterisk;
+      settings = {
 
-      # Networking
-      networks."${networks.front.name}".external = true;
+        # Networking
+        networks."${networks.front.name}".external = true;
 
-           ##########
-      ### # Asterisk # ###
-           ##########
+             ##########
+        ### # Asterisk # ###
+             ##########
 
-      services."${names.asterisk.app}" = {
-        # Image
-        build.image = lib.mkForce pkgs.containerImages.asterisk;
-        # Configuration
-        service = {
-          # Name
-          container_name = names.asterisk.app;
-          # Volumes
-          volumes = [
-            "/data/local/containers/asterisk/config/conf:/etc/asterisk/conf.mine"
-            "/data/local/containers/asterisk/config/voicemail:/var/spool/asterisk/voicemail"
-            "/data/local/containers/asterisk/config/record:/var/spool/asterisk/monitor"
-            "/data/local/containers/asterisk/config/sounds:/var/lib/asterisk/sounds/mine"
-            # Email files
-            "/data/local/mail:/data/local/mail:ro"
-            "/etc/msmtprc:/etc/msmtprc:ro"
-          ];
-          # Networking
-          network_mode = "host";
+        services."${names.asterisk.app}" = {
+          # Image
+          build.image = lib.mkForce pkgs.containerImages.asterisk;
+          # Configuration
+          service = {
+            # Name
+            container_name = names.asterisk.app;
+            # Volumes
+            volumes = [
+              "/data/local/containers/asterisk/config/conf:/etc/asterisk/conf.mine"
+              "/data/local/containers/asterisk/config/voicemail:/var/spool/asterisk/voicemail"
+              "/data/local/containers/asterisk/config/record:/var/spool/asterisk/monitor"
+              "/data/local/containers/asterisk/config/sounds:/var/lib/asterisk/sounds/mine"
+              # Email files
+              "/data/local/mail:/data/local/mail:ro"
+              "/etc/msmtprc:/etc/msmtprc:ro"
+            ];
+            # Networking
+            network_mode = "host";
+          };
         };
-      };
 
-           ##############
-      ### # Asterisk Web # ###
-           ##############
+             ##############
+        ### # Asterisk Web # ###
+             ##############
 
-      services."${names.asterisk.web.normal}" = {
-        # Image
-        build.image = lib.mkForce (pkgs.containerImages.web {});
-        # Options
-        service = {
+        services."${names.asterisk.web.normal}" = {
+          # Image
+          build.image = lib.mkForce (pkgs.containerImages.web {});
+          # Options
+          service = {
+            # Name
+            container_name = names.asterisk.web.normal;
+            # Volumes
+            volumes = [
+              "/data/local/containers/asterisk/config/voicemail:/web/voicemail:ro"
+              "/data/local/containers/asterisk/config/record:/web/monitor:ro"
+            ];
+            # Networking
+            networks = [ networks.front.name ];
+          };
+        };
+
+             #####################
+        ### # Asterisk Web Simple # ###
+             #####################
+
+        services."${names.asterisk.web.simple}".service = {
+          # Image
+          image = "halverneus/static-file-server:latest";
           # Name
-          container_name = names.asterisk.web.normal;
+          container_name = names.asterisk.web.simple;
           # Volumes
           volumes = [
             "/data/local/containers/asterisk/config/voicemail:/web/voicemail:ro"
@@ -272,26 +307,8 @@
           # Networking
           networks = [ networks.front.name ];
         };
+
       };
-
-           #####################
-      ### # Asterisk Web Simple # ###
-           #####################
-
-      services."${names.asterisk.web.simple}".service = {
-        # Image
-        image = "halverneus/static-file-server:latest";
-        # Name
-        container_name = names.asterisk.web.simple;
-        # Volumes
-        volumes = [
-          "/data/local/containers/asterisk/config/voicemail:/web/voicemail:ro"
-          "/data/local/containers/asterisk/config/record:/web/monitor:ro"
-        ];
-        # Networking
-        networks = [ networks.front.name ];
-      };
-
     };
 
     #######
@@ -300,62 +317,74 @@
 
     # NUT Monitor process
 
-    projects.nut.settings = {
+    projects.nut = {
+      serviceName = projects.nut;
+      settings = {
 
-      # Networking
-      networks."${networks.front.name}".external = true;
-
-           #####
-      ### # NUT # ###
-           #####
-
-      services."${names.nut}".service = {
-        # Image
-        image = "teknologist/webnut:latest";
-        # Name
-        container_name = names.nut;
-        # Environment
-        environment = {
-          TZ = config.mine.system.timezone;
-        };
-        env_file = [ "/data/local/containers/nut/nut.env" ];
         # Networking
-        networks = [ networks.front.name ];
-      };
+        networks."${networks.front.name}".external = true;
 
+             #####
+        ### # NUT # ###
+             #####
+
+        services."${names.nut}".service = {
+          # Image
+          image = "teknologist/webnut:latest";
+          # Name
+          container_name = names.nut;
+          # Environment
+          environment = {
+            TZ = config.mine.system.timezone;
+          };
+          env_file = [ "/data/local/containers/nut/nut.env" ];
+          # Networking
+          networks = [ networks.front.name ];
+        };
+
+      };
     };
 
     #######
     # Web #
     #######
 
-    projects.web.settings = {
+    projects.web = {
+      serviceName = projects.web;
+      settings = {
 
-      # Networking
-      networks."${networks.front.name}".external = true;
+        # Networking
+        networks."${networks.front.name}".external = true;
 
-           ######
-      ### # Dash # ###
-           ######
+             ######
+        ### # Dash # ###
+             ######
 
-      services."${names.dash}" = {
-        # Image
-        build.image = lib.mkForce (pkgs.containerImages.web { name = "dashboard"; url = "https://github.com/luis-caldas/personal"; });
-        # Options
-        service = {
-          # Name
-          container_name = names.dash;
-          # Volumes
-          volumes = [
-            "/data/local/containers/dash/config/other.json:/web/other.json:ro"
-          ];
-          # Networking
-          networks = [ networks.front.name ];
+        services."${names.dash}" = {
+          # Image
+          build.image = lib.mkForce (pkgs.containerImages.web { name = "dashboard"; url = "https://github.com/luis-caldas/personal"; });
+          # Options
+          service = {
+            # Name
+            container_name = names.dash;
+            # Volumes
+            volumes = [
+              "/data/local/containers/dash/config/other.json:/web/other.json:ro"
+            ];
+            # Networking
+            networks = [ networks.front.name ];
+          };
         };
-      };
 
+      };
     };
 
   };
+
+  # All the services dependencies
+  systemd.services."${projects.base}".requires = [ "${projects.front}.service" ];
+  systemd.services."${projects.asterisk}".requires = [ "${projects.front}.service" ];
+  systemd.services."${projects.nut}".requires = [ "${projects.front}.service" ];
+  systemd.services."${projects.web}".requires = [ "${projects.front}.service" ];
 
 }
