4c103babd0120fd5d50a03cc3e8cd4b899a43ee0
Luis <luis@caldas.ie>
2024-01-09 10:11:37 +0000

    added a new system

---
+++ systems/vps/default.nix
@@ -0,0 +1,101 @@
+{ pkgs, lib, config, modulesPath, ... }:
+let
+
+  # Network info for
+  networkInfo = {
+    # IPs and ranges
+    host = "10.255.255.254";
+    remote = "10.255.255.1";
+    prefix = 24;
+    # Default Wireguard port
+    port = 123;
+  };
+
+in
+{
+
+  # My own part of configuring
+  mine = {
+    minimal = true;
+    zram = true;
+    boot.override = true;
+    system.hostname = "vps";
+    network.mac = "permanent";
+    user.admin = false;
+    services.ssh = true;
+  };
+
+  # Clear boot configuration
+  boot.loader = lib.mkForce {};
+
+  # Force IP parameters
+  boot.kernel.sysctl."net.ipv6.conf.all.disable_ipv6" = 1;
+  boot.kernel.sysctl."net.ipv6.conf.default.disable_ipv6" = 1;
+  boot.kernel.sysctl."net.ipv4.conf.all.src_valid_mark" = 1;
+
+  # All my imports
+  imports = [
+    # AWS files
+    (modulesPath + "/virtualisation/amazon-image.nix")
+  ];
+
+  # DNS servers
+  networking.networkmanager.insertNameservers = [ "9.9.9.10" ];
+
+  # Disable all ipv6
+  networking.enableIPv6 = false;
+
+  # Firewall setup
+  # The firewall will only work after the NAT
+  networking.firewall = {
+    enable = lib.mkForce true;
+    allowedTCPPorts = [
+      22    # SSH port
+    ];
+    allowedUDPPorts = [
+      123   # Wireguard
+    ];
+  };
+  # Setup Fail 2 Ban
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    ignoreIP = [
+      "127.0.0.0/8"         # Loopback subnet
+      "10.0.0.0/8"          # Local subnet
+      "192.168.0.0/16"      # Local subnet
+      "172.17.0.0/16"       # Docker subnet
+    ];
+  };
+
+  # Disable avahi
+  services.avahi.enable = lib.mkForce false;
+
+  # Set up our wireguard configuration
+  networking.wireguard.interfaces."${networkInfo.interface}" = {
+    ips = [ "${networkInfo.host}/${builtins.toString networkInfo.prefix}" ];
+    listenPort = networkInfo.port;
+    privateKeyFile = "/data/wireguard/host.key";
+    peers = [{
+      publicKey = mfunc.safeReadFile /data/wireguard/remote.pub;
+      presharedKeyFile = "/data/wireguard/shared.key";
+      allowedIPs = [ "${networkInfo.remote}/32" ];
+    }];
+  };
+
+  # User keys for ssh
+  users.users."${config.mine.user.name}".openssh.authorizedKeys.keyFiles = [
+    /etc/nixos/ssh/keys
+  ];
+
+  # Filesystems
+
+  # Add swap
+  swapDevices = [ {
+    device = "/swapfile";
+    size = 4 * 1024;
+  } ];
+
+  system.stateVersion = "23.05";
+
+}
