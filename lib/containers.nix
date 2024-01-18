484f467e54e4ad2c5166cbdde6d8738056f33ef3
Luis <luis@caldas.ie>
2024-01-18 11:54:34 +0000

    removed redundant variable

--- lib/containers.nix
+++ lib/containers.nix
@@ -1,51 +1,46 @@
 { pkgs, lib, config, ... }:
-let
+{
 
-  # Create all the functions that will be available to use
-  allFunctions = {
+  # Converts all the environment items to strings
+  fixEnvironment = builtins.mapAttrs (name: value: builtins.toString value);
 
-    # Converts all the environment items to strings
-    fixEnvironment = builtins.mapAttrs (name: value: builtins.toString value);
-
-    # Create reverse proxy for https on the given container configuration
-    createProxy = info: let
-      # Container internal paths for keys and certificates
-      certPath = "/etc/ssl/custom/default.crt";
-      keyPath = "/etc/ssl/custom/default.key";
-      # The main configuration
-      nginxConfig = pkgs.writeText "custom-nginx-config" ''
-        server {
-            listen ${info.port} ssl http2;
-            server_name _;
-            ssl_certificate ${certPath};
-            ssl_certificate_key ${keyPath};
-            add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload";
-            location / {
-                proxy_set_header Host $host:$server_port;
-                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                proxy_set_header X-Forwarded-Port $server_port;
-                proxy_set_header X-Forwarded-Scheme $scheme;
-                proxy_set_header X-Forwarded-Proto $scheme;
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_pass http://${info.net.ip}:${info.net.port};
-            }
-            ${lib.attrsets.attrByPath [ "extraConfig" ] "" info}
-            error_page 497 https://$host:$server_port$request_uri;
-        }
-      '';
-    in {
-      # Image
-      image = "nginx:latest";
-      # Volumes
-      volumes = [
-        "${nginxConfig}:/etc/nginx/conf.d/default.conf:ro"
-        "${info.ssl.key}:${keyPath}:ro"
-        "${info.ssl.cert}:${certPath}:ro"
-      ];
-      # Networking
-      ports = [ "${info.port}:${info.port}/tcp" ];
-    };
-
+  # Create reverse proxy for https on the given container configuration
+  createProxy = info: let
+    # Container internal paths for keys and certificates
+    certPath = "/etc/ssl/custom/default.crt";
+    keyPath = "/etc/ssl/custom/default.key";
+    # The main configuration
+    nginxConfig = pkgs.writeText "custom-nginx-config" ''
+      server {
+          listen ${info.port} ssl http2;
+          server_name _;
+          ssl_certificate ${certPath};
+          ssl_certificate_key ${keyPath};
+          add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload";
+          location / {
+              proxy_set_header Host $host:$server_port;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Port $server_port;
+              proxy_set_header X-Forwarded-Scheme $scheme;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_pass http://${info.net.ip}:${info.net.port};
+          }
+          ${lib.attrsets.attrByPath [ "extraConfig" ] "" info}
+          error_page 497 https://$host:$server_port$request_uri;
+      }
+    '';
+  in {
+    # Image
+    image = "nginx:latest";
+    # Volumes
+    volumes = [
+      "${nginxConfig}:/etc/nginx/conf.d/default.conf:ro"
+      "${info.ssl.key}:${keyPath}:ro"
+      "${info.ssl.cert}:${certPath}:ro"
+    ];
+    # Networking
+    ports = [ "${info.port}:${info.port}/tcp" ];
   };
 
-in allFunctions
+}
--- lib/functions.nix
+++ lib/functions.nix
@@ -1,67 +1,61 @@
 { lib, ... }:
+rec {
 
-let
+  # Returns a list of the items inside a folder
+  listFilesInFolder = directorySource: builtins.attrNames (builtins.readDir directorySource);
 
-  # Put all functions in attrset
-  allFunctions = rec {
+  # Lists the contents of a folder and creates
+  # a valid link for home manager
+  listCreateLinks = directorySource: directoryDest:
+    let
+      listFiles = listFilesInFolder directorySource;
+    in
+    builtins.listToAttrs (
+      map ( topName: {
+        name = directoryDest + "/" + topName;
+        value.source = directorySource + "/" + topName;
+      }) listFiles
+    );
 
-    # Returns a list of the items inside a folder
-    listFilesInFolder = directorySource: builtins.attrNames (builtins.readDir directorySource);
+  # List the regular files
+  listRegularFiles = directory: (builtins.attrNames (
+    lib.attrsets.filterAttrs
+    (name: value: value == "regular")
+    (builtins.readDir directory)
+  ));
 
-    # Lists the contents of a folder and creates
-    # a valid link for home manager
-    listCreateLinks = directorySource: directoryDest:
-      let
-        listFiles = listFilesInFolder directorySource;
-      in
-      builtins.listToAttrs (
-        map ( topName: {
-          name = directoryDest + "/" + topName;
-          value.source = directorySource + "/" + topName;
-        }) listFiles
-      );
+  # Filters a list of file based on suffix
+  filterFilesExtension = listFiles: extension:
+    builtins.attrNames
+      (lib.attrsets.filterAttrs
+      (name: value: value == "regular" && lib.strings.hasSuffix ".${extension}" name)
+      listFiles);
 
-    # List the regular files
-    listRegularFiles = directory: (builtins.attrNames (
-      lib.attrsets.filterAttrs
-      (name: value: value == "regular")
-      (builtins.readDir directory)
-    ));
+  listAllSuffixFiles = directoryPath: extension:
+    map (each: "${directoryPath}/${each}") (filterFilesExtension (builtins.readDir directoryPath) extension);
 
-    # Filters a list of file based on suffix
-    filterFilesExtension = listFiles: extension:
-      builtins.attrNames
-        (lib.attrsets.filterAttrs
-        (name: value: value == "regular" && lib.strings.hasSuffix ".${extension}" name)
-        listFiles);
+  listAllSuffixFilesRecursive = directoryPath: extension:
+    filterFilesExtension (lib.filesystem.listFilesRecursive directoryPath) extension;
 
-    listAllSuffixFiles = directoryPath: extension:
-      map (each: "${directoryPath}/${each}") (filterFilesExtension (builtins.readDir directoryPath) extension);
-
-    listAllSuffixFilesRecursive = directoryPath: extension:
-      filterFilesExtension (lib.filesystem.listFilesRecursive directoryPath) extension;
-
-    # Capitalises first and anything after a space
-    capitaliseString = inputString: let
-        splitChar = " ";
-        listStrings = lib.strings.splitString splitChar inputString;
-        capitalisedList = map (eachWord: let
-            splitWord = lib.strings.stringToCharacters eachWord;
-            firstChar = builtins.head splitWord;
-            restString = lib.strings.concatStrings (lib.lists.drop 1 splitWord);
-            newWord = (lib.strings.toUpper firstChar) + restString;
-          in newWord) listStrings;
-        properName = lib.strings.concatStringsSep splitChar capitalisedList;
-      in properName;
+  # Capitalises first and anything after a space
+  capitaliseString = inputString: let
+      splitChar = " ";
+      listStrings = lib.strings.splitString splitChar inputString;
+      capitalisedList = map (eachWord: let
+          splitWord = lib.strings.stringToCharacters eachWord;
+          firstChar = builtins.head splitWord;
+          restString = lib.strings.concatStrings (lib.lists.drop 1 splitWord);
+          newWord = (lib.strings.toUpper firstChar) + restString;
+        in newWord) listStrings;
+      properName = lib.strings.concatStringsSep splitChar capitalisedList;
+    in properName;
 
 
-    # Function for safely reading a file
-    safeReadFile = filePath:
-      if (builtins.pathExists filePath) then
-        lib.removeSuffix "\n" (builtins.readFile filePath)
-      else
-        "";
+  # Function for safely reading a file
+  safeReadFile = filePath:
+    if (builtins.pathExists filePath) then
+      lib.removeSuffix "\n" (builtins.readFile filePath)
+    else
+      "";
 
-  };
-
-in allFunctions
+}
