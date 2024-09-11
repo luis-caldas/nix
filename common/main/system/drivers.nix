78e86d78e5ce77bef615858d0f717456fe362ff1
Luis <luis@caldas.ie>
2024-09-11 14:25:39 +0100

    reorganised drivers file

--- common/main/system/drivers.nix
+++ common/main/system/drivers.nix
@@ -1,15 +1,18 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 {
 
   # Drivers to install on the system
   boot.extraModulePackages = with config.boot.kernelPackages; [
 
-      # Wifi
-      rtw88
+    # Wifi
+    rtw88
 
-      # Video loopback
-      v4l2loopback
+    # Video loopback
+    v4l2loopback
 
   ];
+
+  # Firmware to install on the system
+  hardware.firmware = with pkgs; lib.mkBefore [];
 
 }
