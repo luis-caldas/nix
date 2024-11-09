dca80f2a9d7dadcd7a73edf8e58f2ac42d997e72
Luis <luis@caldas.ie>
2024-11-09 17:03:48 +0000

    removed dns_search from container service

--- systems/router/containers/vpn.nix
+++ systems/router/containers/vpn.nix
@@ -14,48 +14,39 @@ with shared;
   ### # WireGuard # ###
        ###########
 
-  services."${names.wire}" = {
+  services."${names.wire}".service = {
 
-    service = {
+    # Image file
+    image = "lscr.io/linuxserver/wireguard:latest";
 
-      # Image file
-      image = "lscr.io/linuxserver/wireguard:latest";
-
-      # Environments
-      environment = let
-        subnet = "${pkgs.networks.vpn.network}/${builtins.toString pkgs.networks.vpn.prefix}";
-      in pkgs.functions.container.fixEnvironment {
-        TZ = config.mine.system.timezone;
-        PUID = config.mine.user.uid;
-        GUID = config.mine.user.gid;
-        INTERNAL_SUBNET = subnet;
-        ALLOWEDIPS = "0.0.0.0/0,${pkgs.networks.docker.dns.vpn.wire.ip}/32,${subnet},${pkgs.networks.internal}";
-        PEERS = listUsers;
-        SERVERPORT = pkgs.networks.ports.open;
-        PERSISTENTKEEPALIVE_PEERS = "all";
-      };
-      env_file = [ "/data/local/containers/wireguard/env/wire.env" ];
-
-      # Volumes
-      volumes = [
-        "/data/local/containers/wireguard/config:/config"
-      ];
-
-      # Networking
-      ports = [
-        "${builtins.toString pkgs.networks.ports.open}:${builtins.toString pkgs.networks.ports.wireguard}/udp"
-      ];
-      networks = [ networks.wire ];
-      capabilities.NET_ADMIN = true;
-      sysctls = {
-        "net.ipv4.conf.all.src_valid_mark" = 1;
-      };
-
+    # Environments
+    environment = let
+      subnet = "${pkgs.networks.vpn.network}/${builtins.toString pkgs.networks.vpn.prefix}";
+    in pkgs.functions.container.fixEnvironment {
+      TZ = config.mine.system.timezone;
+      PUID = config.mine.user.uid;
+      GUID = config.mine.user.gid;
+      INTERNAL_SUBNET = subnet;
+      ALLOWEDIPS = "0.0.0.0/0,${pkgs.networks.docker.dns.vpn.wire.ip}/32,${subnet},${pkgs.networks.internal}";
+      PEERS = listUsers;
+      SERVERPORT = pkgs.networks.ports.open;
+      PERSISTENTKEEPALIVE_PEERS = "all";
     };
+    env_file = [ "/data/local/containers/wireguard/env/wire.env" ];
 
-    # Extra for dns name search
-    out.service = {
-      dns_search = ".";
+    # Volumes
+    volumes = [
+      "/data/local/containers/wireguard/config:/config"
+    ];
+
+    # Networking
+    ports = [
+      "${builtins.toString pkgs.networks.ports.open}:${builtins.toString pkgs.networks.ports.wireguard}/udp"
+    ];
+    networks = [ networks.wire ];
+    capabilities.NET_ADMIN = true;
+    sysctls = {
+      "net.ipv4.conf.all.src_valid_mark" = 1;
     };
 
   };
