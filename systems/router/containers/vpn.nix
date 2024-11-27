7a2a9fc82335462f236456d5517bfe032e726df6
Luis <luis@caldas.ie>
2024-11-27 19:15:37 +0000

    updated vpn once again

--- systems/router/containers/vpn.nix
+++ systems/router/containers/vpn.nix
@@ -10,27 +10,47 @@ with shared;
     networks.vpn
   ];
 
-       #########
-  ### # OpenVPN # ###
-       #########
+       ###########
+  ### # Wireguard # ###
+       ###########
 
   services."${names.vpn}".service = {
 
     # Image file
-    image = "openvpn/openvpn-as:latest";
+    image = "ghcr.io/wg-easy/wg-easyas:latest";
+
+    # Environments
+    environment = pkgs.functions.container.fixEnvironment {
+      LANG = "en";
+      PORT = 80;  # WebUI Port
+      WG_PORT = builtins.toString pkgs.networks.ports.open;  # VPN Port
+      WG_CONFIG_PORT = builtins.toString pkgs.networks.ports.open;
+      WG_PERSISTENT_KEEPALIVE = 25;
+      WG_DEFAULT_ADDRESS = "10.200.0.x";
+      WG_DEFAULT_DNS = builtins.head pkgs.networks.dns;
+      UI_TRAFFIC_STATS = true;
+      UI_CHART_TYPE = 1;
+    };
+    env_file = [ "/data/local/containers/wireguard/env/wire.env" ];
 
     # Volumes
     volumes = [
-      "/data/local/containers/openvpn/config:/openvpn"
+      "/data/local/containers/wireguard/config:/etc/wireguard"
     ];
 
     # Networking
     ports = [
-      "${builtins.toString pkgs.networks.ports.free}:${builtins.toString pkgs.networks.ports.free}/tcp"
       "${builtins.toString pkgs.networks.ports.open}:${builtins.toString pkgs.networks.ports.open}/udp"
     ];
     networks = [ networks.vpn ];
-    capabilities.NET_ADMIN = true;
+    capabilities = {
+      NET_ADMIN = true;
+      SYS_MODULE = true;
+    };
+    sysctls = {
+      "net.ipv4.conf.all.src_valid_mark" = 1;
+      "net.ipv4.ip_forward" = 1;
+    };
 
   };
 
