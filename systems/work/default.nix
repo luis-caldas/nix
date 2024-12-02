d1d182591712dbaa84bd6688b9f7ff1a3e56e17b
Luis <luis@caldas.ie>
2024-12-02 18:06:21 +0000

    added automatic egpu switch on boot for work

--- systems/work/default.nix
+++ systems/work/default.nix
@@ -23,6 +23,43 @@
     ENV{DEVNAME}=="/dev/dri/card1", TAG+="mutter-device-preferred-primary"
   '';
 
+  # Stop iGPU from starting if eGPU present
+  systemd.services.gpuer = let
+    script = pkgs.writeShellApplication {
+      name = "gpu-script";
+      runtimeInputs = with pkgs; [ pciutils gawk gnugrep ];
+      text = ''
+        # Addresses
+        addr_igpu="0000:c1:00.0"
+        # driver_igpu="amdgpu"
+        addr_egpu="0000:66:00.0"
+        # Remove if eGPU present
+        if lspci -D -d ::0300 -n | awk -F' ' '{print $1}' | grep -q "''${addr_egpu}"; then
+          # Verbose
+          echo eGPU Used
+          # Remove iGPU
+          echo 1 > "/sys/bus/pci/devices/''${addr_igpu}/remove"
+        else
+          # Verbose
+          echo iGPU Used
+        fi
+      '';
+    };
+  in {
+    description = "Disable iGPU when eGPU present";
+    after = [ "bolt.service" "display-manager.service" ];
+    serviceConfig = {
+      ExecStart = [ "${script}/bin/gpu-script" ];
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    wantedBy = [ "graphical.target" ];
+    restartIfChanged = false;
+  };
+
+  # RGB Control
+  services.hardware.openrgb.enable = false;
+
   # Disable fingerprint
   services.fprintd.enable = false;
 
