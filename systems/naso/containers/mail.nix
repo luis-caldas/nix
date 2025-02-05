8601710b48c727a5c749bfaba891ace1d8441212
Luis <luis@caldas.ie>
2025-02-05 20:10:03 +0000

    updated postfix limit

--- systems/naso/containers/mail.nix
+++ systems/naso/containers/mail.nix
@@ -32,7 +32,7 @@ with shared;
       ENABLE_CLAMAV = 1;
       ENABLE_FAIL2BAN = 1;
       # Limits
-      POSTFIX_MESSAGE_SIZE_LIMIT = 100;
+      POSTFIX_MESSAGE_SIZE_LIMIT = 102400000;
       # SSL
       SSL_TYPE = "manual";
       SSL_KEY_PATH = "/ssl/main.key";
