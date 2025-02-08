68acde2ed9c0b63cc871325b979e449e29d1ccc2
Luis <luis@caldas.ie>
2025-02-08 14:15:27 +0000

    updated the emojis font

--- common/manager/user/video/ecosystem.nix
+++ common/manager/user/video/ecosystem.nix
@@ -41,7 +41,7 @@ lib.mkIf osConfig.mine.graphics.enable
       courier-prime
       apl386 bqn386
       sarasa-gothic
-      noto-fonts-emoji-blob-bin
+      noto-fonts-monochrome-emoji
     ];
 
     # Icons
