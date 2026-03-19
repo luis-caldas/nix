6d930cd98ca950ec8a4c4870f76d586c92e56d97
Luis <luis@caldas.ie>
2026-03-19 19:01:19 +0000

    updated list on naso to better version

--- systems/naso/containers/list.nix
+++ systems/naso/containers/list.nix
@@ -13,7 +13,7 @@ with shared;
   ### # List # ###
        ######
 
-  services."${names.list}".service = {
+  services."${names.list}-old".service = {
     # Image
     image = "wingysam/christmas-community:latest";
     # Environment
@@ -38,8 +38,36 @@ with shared;
     };
     # Volumes
     volumes = [
-      "/data/bunker/data/containers/list:/data"
+      "/data/bunker/data/containers/list/old:/data"
     ];
+    # Networking
+    networks = [
+      networks.list
+    ];
+  };
+
+       ######
+  ### # List # ###
+       ######
+
+  services."${names.list}".service = {
+    # Image
+    image = "ghcr.io/cmintey/wishlist:latest";
+
+    # Environment
+    environment = pkgs.functions.container.fixEnvironment {
+      TOKEN_TIME = "72";  # Hours
+      DEFAULT_CURRENCY = "EUR";
+      MAX_IMAGE_SIZE = "26214400";  # 25 MiB
+    };
+    env_file = [ "/data/local/containers/list/list.env" ];
+
+    # Volumes
+    volumes = [
+      "/data/bunker/data/containers/list/uploads:/usr/src/app/uploads"
+      "/data/bunker/data/containers/list/data:/usr/src/app/data"
+    ];
+
     # Networking
     networks = [
       networks.list
