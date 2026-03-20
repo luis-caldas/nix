abbeb5217ad5dadcb044d75a7bab74624b12ed49
Luis <luis@caldas.ie>
2026-03-20 20:51:29 +0000

    went away with old list

--- systems/naso/containers/list.nix
+++ systems/naso/containers/list.nix
@@ -13,43 +13,6 @@ with shared;
   ### # List # ###
        ######
 
-  services."${names.list}-old".service = {
-    # Image
-    image = "wingysam/christmas-community:latest";
-    # Environment
-    environment = pkgs.functions.container.fixEnvironment {
-      # Listing
-      TABLE = true;
-      MARKDOWN = false;
-      SINGLE_LIST = false;
-      LISTS_PUBLIC = false;
-      # Node
-      NODE_OPTIONS = "--max-http-header-size=32768";
-      # Visual
-      SITE_TITLE = "Present List";
-      SHORT_TITLE = "List";
-      BULMASWATCH = "cyborg";
-      # Networking
-      PORT = 80;
-      TRUST_PROXY = "10.0.0.0/8";
-      UPLOAD_PFP_MAX_SIZE = "25";
-      # Update
-      UPDATE_CHECK = true;
-    };
-    # Volumes
-    volumes = [
-      "/data/bunker/data/containers/list/old:/data"
-    ];
-    # Networking
-    networks = [
-      networks.list
-    ];
-  };
-
-       ######
-  ### # List # ###
-       ######
-
   services."${names.list}".service = {
     # Image
     image = "ghcr.io/cmintey/wishlist:latest";
