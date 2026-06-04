118ae1f9e7862c66d2df83736a54228c315305f6
Luis <luis@caldas.ie>
2026-06-04 11:37:57 +0100

    rtw88 not as minimal

--- common/main/system/drivers.nix
+++ common/main/system/drivers.nix
@@ -1,14 +1,13 @@
 { config, pkgs, lib, ... }:
 {
 
-  boot.extraModulePackages = with config.boot.kernelPackages;
-    lib.optionals (!config.mine.minimal) [
-      # WiFi
-      rtw88
-    ] ++ lib.optionals config.mine.graphics.enable [
-      # Video loopback
-      v4l2loopback
-    ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    # WiFi
+    rtw88
+  ] ++ lib.optionals config.mine.graphics.enable [
+    # Video loopback
+    v4l2loopback
+  ];
 
   hardware.firmware = with pkgs; lib.mkBefore [];
 
