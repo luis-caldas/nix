4a3ebdb03df1626d465e5aecef97e1cc69d28bd6
Luis <luis@caldas.ie>
2026-06-04 11:58:33 +0100

    remove not needed driver

--- common/main/system/drivers.nix
+++ common/main/system/drivers.nix
@@ -2,8 +2,7 @@
 {
 
   boot.extraModulePackages = with config.boot.kernelPackages; [
-    # WiFi
-    rtw88
+    #
   ] ++ lib.optionals config.mine.graphics.enable [
     # Video loopback
     v4l2loopback
